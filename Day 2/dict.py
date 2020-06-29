0a0d880c5d2c428372ab4180bd185ca16a5964b2
iparinile <poselennov.il@ya.ru>
2020-06-29 16:38:35 +0500

    Повтарение словарей и файлов

--- Day 2/dict.py
+++ Day 2/dict.py
@@ -1,1 +1,37 @@
-# 147
+bob1 = dict(name='Bob', job='dev', age=40)  # Как еще можно создать словарь
+print(bob1)  # {'name': 'Bob', 'job': 'dev', 'age': 40}
+
+bob2 = dict(zip(['name', 'job', 'age'], ['Bob', 'dev', 40]))  # То же самое
+print(bob2)
+
+rec = {'name': {'first': 'Bob', 'last': 'Smith'},
+       'jobs': ['dev', 'mgr'],
+       'age': 40.5}
+print(rec['name'])  # 'name' - вложенный словарь
+# {'first': 'Bob', 'last': 'Smith'}
+print(rec['name']['last'])  # Индексация во вложенном словаре
+# Smith
+print(rec['jobs'])  # 'jobs' - вложенный список
+# ['dev', 'mgr']
+print(rec['jobs'][-1])  # Индексация
+# mgr
+rec['jobs'].append('janitor')  # Расширение списка названий должностей на месте
+print(rec)  # {'name': {'first': 'Bob', 'last': 'Smith'}, 'jobs': ['dev', 'mgr', 'janitor'], 'age': 40.5}
+
+D = {'a': 1, 'b': 2, 'c': 3}
+print('f' in D)  # False
+if 'f' not in D:
+    print('missing')
+# missing
+
+Ks = list(D.keys())  # Неупорядоченный список ключей
+print(Ks)  # ['a', 'b', 'c']
+Ks.sort()  # Отсортированный список ключей
+for key in Ks:  # Проход по отсортированным ключам
+    print(key, '=>', D[key])
+# a => 1
+# b => 2
+# c => 3
+# Альтернативный способ:
+for key in sorted(D):
+    print(key, '=>', D[key])
---
+++ Day 2/files.py
@@ -0,0 +1,29 @@
+f = open('data.txt', 'w')  # Создать новый файл в режиме записи ('w')
+f.write('Hello\n')  # Записать в него строки символов
+f.write('world\n')
+f.close()  # Закрыть для сбрасывания буферов вывода на диск
+
+f = open('data.txt', 'r')  # 'r' (чтение) - стандартный режим обработки
+text = f.read()  # Прочитать все содержимое файла в строку
+print(text)
+# Hello
+# world
+print(text.split())  # Содержимое файла - всегда строка
+# ['Hello', 'world']
+for line in open('data.txt'):
+    print(line)
+
+import struct
+packed = struct.pack('>i4sh', 7, b'spam', 8)  # Создание упакованных двоичных данных
+print(packed)  # b'\x00\x00\x00\x07spam\x00\x08'
+file = open('data.bin', 'wb')
+file.write(packed)
+file.close()
+
+data = open('data.bin', 'rb').read()
+print(data)  # b'\x00\x00\x00\x07spam\x00\x08'
+print(data[4:8])  # b'spam'
+print(list(data))  # [0, 0, 0, 7, 115, 112, 97, 109, 0, 8]
+print(struct.unpack('>i4sh', data))  # (7, b'spam', 8)
+
+
